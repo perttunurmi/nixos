3d82b2058fc2c4a3e125c897c96f004253959360
Perttu Nurmi <perttu.nurmi@gmail.com>
2025-07-05 19:17:20 +0300

    Fujitsu

--- flake.nix
+++ flake.nix
@@ -104,6 +104,35 @@
             }
           ];
         };
+
+        Fujitsu =
+          let
+            username = "perttu";
+            specialArgs = {
+              inherit username;
+              inherit inputs;
+            };
+          in
+          nixpkgs.lib.nixosSystem {
+            inherit specialArgs;
+            system = "x86_64-linux";
+            modules = [
+              ./hosts/Fujitsu
+              ./users/${username}/nixos.nix
+              # make home-manager as a module of nixos
+              # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
+              home-manager.nixosModules.home-manager
+              {
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+                home-manager.backupFileExtension = "bak";
+                home-manager.extraSpecialArgs = inputs // specialArgs;
+                home-manager.users.${username} = import ./users/${username}/home.nix;
+              }
+            ];
+          };
+
+      };
     };
   };
 }
--- hosts/Fujitsu/hardware-configuration.nix
+++ hosts/Fujitsu/hardware-configuration.nix
@@ -8,34 +8,18 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usbhid" ];
+  boot.initrd.availableKernelModules = [ "ata_generic" "uhci_hcd" "ehci_pci" "ata_piix" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/2048a7ca-7971-41f9-a07a-b2b8bd816e42";
+    { device = "/dev/disk/by-uuid/952f400c-f742-46ae-8935-cd2597772833";
       fsType = "ext4";
     };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/377A-5F3A";
-      fsType = "vfat";
-      options = [ "fmask=0077" "dmask=0077" ];
-    };
-
-  fileSystems."/home/perttu/dropbox" =
-    { device = "dropbox:";
-      fsType = "fuse.rclone";
-    };
-
-  fileSystems."/home/perttu/google" =
-    { device = "google:";
-      fsType = "fuse.rclone";
-    };
-
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/1ce49921-3e6c-4516-9c9a-f764d77190ba"; }
+    [ { device = "/dev/disk/by-uuid/dc8308de-97ac-4d9f-8e50-4faf576f0fb4"; }
     ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
@@ -43,8 +27,7 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp61s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp0s25.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
