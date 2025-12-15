0d457d0c947e746644b6ef42ac62e166d3fd69dc
Perttu Nurmi <perttu.nurmi@gmail.com>
2025-12-15 20:38:09 +0200

    Add Atuin

--- home/common/default.nix
+++ home/common/default.nix
@@ -97,4 +97,14 @@ in {
       vim_keys = true;
     };
   };
+
+  programs.atuin = {
+    enable = true;
+    settings = {
+      auto_sync = true;
+      sync_frequency = "5m";
+      sync_address = "https://api.atuin.sh";
+      search_mode = "fuzzy";
+    };
+  };
 }
